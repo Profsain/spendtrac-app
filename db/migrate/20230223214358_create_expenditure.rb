c3b1b6b8b2da8063de5c9e7b54396b2543fd7195
Profsain <profsaincrown@gmail.com>
2023-02-23 21:54:16 +0000

    added app models

---
+++ db/migrate/20230223214358_create_expenditure.rb
@@ -0,0 +1,10 @@
+class CreateExpenditure < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenditures do |t|
+      t.string :name
+      t.integer :amount
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end

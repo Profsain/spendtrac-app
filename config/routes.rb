171081772c522e650a971f328a22ef0adb6775cc
Profsain <profsaincrown@gmail.com>
2023-02-23 22:08:16 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index"  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'categories#index'
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :expenditure, only: [:index]
+  end
+
+  resources :expenditures, only: [:new, :create, :destroy]
 end
